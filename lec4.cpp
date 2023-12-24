9f3ea5442f3b5039a3e2e1bcd47add5eb862bc07
Abhishek Anand <112304737+abhishekanandok@users.noreply.github.com>
2023-12-24 12:10:52 +0530

    2nd

---
+++ lec4.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip> //used for setw
+
+using namespace std;
+
+int main()
+{
+    // Manipulators in C++
+    // int a =3, b=78, c=1233;
+    // cout<<"The value of a without setw is: "<<a<<endl;
+    // cout<<"The value of b without setw is: "<<b<<endl;
+    // cout<<"The value of c without setw is: "<<c<<endl;
+
+    // cout<<"The value of a is: "<<setw(4)<<a<<endl;
+    // cout<<"The value of b is: "<<setw(4)<<b<<endl;
+    // cout<<"The value of c is: "<<setw(4)<<c<<endl;
+
+    // Operator Precedence
+    int a = 3, b = 4;
+    // int c = (a*5)+b;
+    int c = ((((a * 5) + b) - 45) + 87);
+    cout << c;
+
+
+
+
+
+
+    int age;
+    cout << "Tell me your age" << endl;
+    cin >> age;
+    // 2. Selection control structure: Switch Case statements
+    switch (age)
+    {
+    case 18:
+        cout << "You are 18" << endl;
+        break;
+    case 22:
+        cout << "You are 22" << endl;
+        break;
+    case 2:
+        cout << "You are 2" << endl;
+        break;
+
+    default:
+        cout << "No special cases" << endl;
+        break;
+    }
+
+    cout << "Done with switch case";
+    return 0;
+}
